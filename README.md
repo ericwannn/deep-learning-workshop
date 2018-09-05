7b4f67839f4438b6296d60990b4af8491e8fe317
Eric Wan <ericwannn@foxmail.com>
2018-09-05 20:45:33 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # deep-learning-workshop
-papers/notes/codes on popluar deep learning methods
+
+Here are some awesome papers/tutorials on machine/deep learning and related fields, along with some notes and codes if applied.  
+You are always welcomed to share your own thoughts here with us !
+
+I roughly categorise the resources into paper and tutorial (perhaps more in the future) and tag the resourses with either of these two. The original pdf of papers are stored locally only, whereas a scirpt, `sync.py`, was created to download them using `wget`. 
+
+**Usage**:
+
+To run this script, simply do a `python sync.py` which downloads the papers (urls of resources with tag `[paper]` in their titles) to the according directory. 
+
+Please make sure that you have `Python` and `wget` installed and configured properly. The script should run properly on Windows, macOS and most Linux distributions.
+
+**PR**:
+
+Everything that is interesting is welcomed! Please follow the same pattern which might make things eaiser:
+
+* The type of the resource is defined in tags together with its title in header 4 (####)
+* The notes and codes are stored in separate files in the same folder
+* Elements like url, links to the notes and codes are placed in bullet points under its title (header 4)
+
+For example, a typical organisation of resources would be sth as follows.
+
+```markdown
+## natural language processing
+
+### text classification
+
+#### [paper] Convolutional Neural Networks for Sentence Classification
+
+* [url](http://aclweb.org/anthology/D/D14/D14-1181.pdf)
+* [notes](/natural_language_processing/text_classification/Convolutional_Neural_Networks_for_Sentence_Classification.md)
+* codes
+
+#### [tutorial] Home Credit Default Risk 1st Place Solution
+
+* [url](https://www.kaggle.com/c/home-credit-default-risk/discussion/64821)
+```
+
+## natural language processing
+
+### text classification
+
+#### [paper] Convolutional Neural Networks for Sentence Classification
+
+* [url](http://aclweb.org/anthology/D/D14/D14-1181.pdf)
+* [notes](/natural_language_processing/text_classification/Convolutional_Neural_Networks_for_Sentence_Classification.md)
+* codes
+
+#### [paper] Document Modeling with Gated Recurrent Neural Network
+
+* [url](https://www.cs.cmu.edu/~ark/EMNLP-2015/proceedings/EMNLP/pdf/EMNLP167.pdf)
+* notes
+* codes
+
+#### [paper] Hierarchical Attention Networks for Document Classification
+
+* [url](http://aclweb.org/anthology/N/N16/N16-1174.pdf)
+* notes
+* codes
+
+## computer vision
---
+++ sync.py
@@ -0,0 +1,79 @@
+# coding=utf-8
+from __future__ import print_function
+import os
+import platform
+import shlex
+import datetime
+import subprocess
+import time
+from collections import namedtuple
+ 
+def execute_command(cmdstring, cwd=None, timeout=None, shell=False):
+    """Execute a shell command. Encapsulate subprocess.Popen. Support timeout and stdout/stderr
+
+    @Parameter: 
+        cmdstring:  command
+        cwd:        change the directory to cwd if it's set
+        timeout:    in second
+        shell:      if the command is executed by shell
+
+    @Return:        returncode
+    @Raises:        Exception: timeout
+    """
+    cmdstring_list = cmdstring if shell else shlex.split(cmdstring)
+    if timeout:
+        end_time = datetime.datetime.now() + datetime.timedelta(seconds=timeout)
+    
+    sub = subprocess.Popen(
+        cmdstring_list, cwd=cwd, stdin=subprocess.PIPE,
+        shell=shell, bufsize=4096
+        )
+    
+    while sub.poll() is not None:
+        time.sleep(0.1)
+        if timeout:
+            if end_time <= datetime.datetime.now():
+                raise Exception("Timeout：{}".format(cmdstring))
+            
+    return str(sub.returncode)
+
+def download_file(url, file_name, directory):
+    execute_command(
+        "wget {} -O {}".format(url, file_name),
+        cwd=directory,
+        shell=True
+        )
+
+def sync_papers():
+    current_path = os.getcwd()
+    system = platform.system().lower()
+    assert system in ["windows", "linux", "darwin"]
+    download_list = []
+    with open("README.md") as f:
+        for line in f.readlines():
+            sline = line.strip().split()
+            if not len(sline): 
+                continue
+            if sline[0] == "##":
+                h2 = "_".join(sline[1:])
+            elif sline[0] == "###":
+                h3 = "_".join(sline[1:])
+            elif sline[0] == "####":
+                file_name = "_".join(sline[1:]) + ".pdf"
+            elif sline[1].startswith("[url]"):
+                url  = line[8:-2]
+                if system == "windows":
+                    directory = current_path + "\\{}\\{}\\".format(h2, h3)
+                else:
+                    directory = current_path + "/{}/{}/".format(h2, h3)
+                
+                if file_name not in os.listdir(directory):
+                    download_file(
+                        url, file_name, directory
+                        )
+                else:
+                    print("The file {} already exists!".format(file_name))
+
+
+if __name__=="__main__":
+    sync_papers()
